e661f1ec46ed93b1ab68b1685dbaf73006067df8
JanWielgus <jan.wielgus12@gmail.com>
2020-07-30 13:54:12 +0200

    Created Vector3 file with int16 and float vector types

---
+++ ColyberCopter/Common/Vector3.h
@@ -0,0 +1,28 @@
+/**
+ * @file Vector3.h
+ * @author Jan Wielgus
+ * @date 2020-07-30
+ * 
+ */
+
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+
+struct vector3Float
+{
+    float x;
+    float y;
+    float z;
+};
+
+
+struct vector3Int16
+{
+    int16_t x;
+    int16_t y;
+    int16_t z;
+};
+
+
+#endif
