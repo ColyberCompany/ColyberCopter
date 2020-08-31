c4492ba15262e47bfe5c06ec50e7d1f38b443784
Jan Wielgus <jan.wielgus12@gmail.com>
2020-08-31 19:53:25 +0200

    Implementation of base class of Madgwick algorithms

---
+++ ColyberCopter/PositionAndRotation/RotationCalculation/MadgwickBase.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file MadgwickBase.cpp
+ * @author Jan Wielgus
+ * @date 2020-08-31
+ * 
+ */
+
+#include "MadgwickBase.h"
+
+
+const float MadgwickBase::DefaultBeta = 0.1;
+
+
+MadgwickBase::MadgwickBase(float sampleFrequency, float beta = DefaultBeta)
+{
+    invSampleFreq = 1.0f / sampleFrequency;
+    this->beta = beta;
+    q0 = 1.0f;
+	q1 = 0.0f;
+	q2 = 0.0f;
+	q3 = 0.0f;
+}
+
+
+float MadgwickBase::invertedSquareRoot(float x)
+{
+    float halfx = 0.5f * x;
+	float y = x;
+	long i = *(long*)&y;
+	i = 0x5f3759df - (i >> 1);
+	y = *(float*)&i;
+	y = y * (1.5f - (halfx * y * y));
+	y = y * (1.5f - (halfx * y * y));
+	return y;
+}
--- ColyberCopter/PositionAndRotation/RotationCalculation/MadgwickBase.h
+++ ColyberCopter/PositionAndRotation/RotationCalculation/MadgwickBase.h
@@ -1,1 +1,44 @@
-// TODO: implement madgwick base (copy from STM_FlightController)
+/**
+ * @file MadgwickBase.h
+ * @author Jan Wielgus
+ * @brief Base class for Madgwick IMU and AHRS classes.
+ * @date 2020-08-31
+ * 
+ */
+
+#ifndef MADGWICKBASE_H
+#define MADGWICKBASE_H
+
+
+class MadgwickBase
+{
+protected:
+    static const float DefaultBeta;
+	float beta; // algorithm gain (2 * proportional gain)
+
+	float q0;
+	float q1;
+	float q2;
+	float q3; // quaternion of sensor frame relative to auxiliary frame
+	float invSampleFreq;
+
+	float recipNorm;
+	float s0, s1, s2, s3;
+	float qDot1, qDot2, qDot3, qDot4;
+
+public:
+    MadgwickBase(float sampleFrequency, float beta = DefaultBeta);
+    virtual ~MadgwickBase();
+
+    /**
+     * @brief Fast inverse square-root
+     * See: http://en.wikipedia.org/wiki/Fast_inverse_square_root.
+     * 
+     * @param x Number which square root will be extracted.
+     * @return Square root of parameter x.
+     */
+    static float invertedSquareRoot(float x);
+};
+
+
+#endif
