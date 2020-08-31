4891fb3d44ea5b6789468a77809ab6208e978aa8
Jan Wielgus <jan.wielgus12@gmail.com>
2020-09-01 01:21:42 +0200

    Added new interface: IPositionCalculation
    This interface combines horizontal position calculation and altitude calculation interfaces

---
+++ ColyberCopter/Interfaces/IPositionCalculation.h
@@ -0,0 +1,25 @@
+/**
+ * @file IPositionCalculation.h
+ * @author Jan Wielgus
+ * @brief Interface for position calculation classes.
+ * @date 2020-09-01
+ * 
+ */
+
+#ifndef IPOSITIONCALCULATION_H
+#define IPOSITIONCALCULATION_H
+
+
+class IPositionCalculation
+{
+public:
+    virtual ~IPositionCalculation() {}
+
+    virtual void updatePositionCalculation() = 0;
+    virtual double getLongitude_deg() = 0;
+    virtual double getLatitude_deg() = 0;
+    virtual float getAltitude_m() = 0;
+};
+
+
+#endif
