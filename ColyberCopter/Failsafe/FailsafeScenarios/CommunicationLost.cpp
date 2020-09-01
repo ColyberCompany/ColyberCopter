8f2f13443e1321440ad8a00400e6ac3946dbfe14
Jan Wielgus <jan.wielgus12@gmail.com>
2020-09-01 22:47:58 +0200

    CommunicationLost failsafe event in progress

---
+++ ColyberCopter/Failsafe/FailsafeScenarios/CommunicationLost.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file CommunicationLost.cpp
+ * @author Jan Wielgus
+ * @date 2020-09-01
+ * 
+ */
+
+#include "CommunicationLost.h"
+
+
+CommunicationLost::CommunicationLost()
+{
+
+}
+
+
+void CommunicationLost::execute()
+{
+    
+}
--- ColyberCopter/Failsafe/FailsafeScenarios/CommunicationLost.h
+++ ColyberCopter/Failsafe/FailsafeScenarios/CommunicationLost.h
@@ -1,1 +1,27 @@
-// TODO: implement CommunicationLost failsafe class
+/**
+ * @file CommunicationLost.h
+ * @author your name (you@domain.com)
+ * @brief Failsafe scenario that take action when communication is lost.
+ * @date 2020-09-01
+ * 
+ */
+
+#ifndef COMMUNICATIONLOST_H
+#define COMMUNICATIONLOST_H
+
+#include <IExecutable.h>
+// TODO: create way to disarm motors, this something have to consider different motors configuration
+
+
+class CommunicationLost : public IExecutable
+{ // TODO: implement CommunicationLost failsafe class.
+public:
+    CommunicationLost();
+    void execute() override;
+
+private:
+    bool getCommunicationState();
+};
+
+
+#endif
