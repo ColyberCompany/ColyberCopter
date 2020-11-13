95a776903750f8e37b483c8c4601d3c09b90382e
JanWielgus <jan.wielgus12@gmail.com>
2020-11-13 09:21:49 +0100

    VirtualPilot class is ready for testing

--- ColyberCopter/VirtualPilot.cpp
+++ ColyberCopter/VirtualPilot.cpp
@@ -20,6 +20,47 @@ VirtualPilot::VirtualPilot(IMotors* motors, FlightMode* initialFlightMode, const
 }
 
 
+bool VirtualPilot::addFlightMode(FlightMode* flightMode)
+{
+    Iterator<FlightMode*>* iterator = flightModesArray.getIterator();
+
+    while (iterator->hasNext())
+        if (iterator->next()->getType() == flightMode->getType())
+            return false; // flight mode instance of this type was already added
+
+    flightModesArray.add(flightMode);
+    return true;
+}
+
+
+bool VirtualPilot::initializeFlightModes()
+{
+    Iterator<FlightMode*>* iterator = flightModesArray.getIterator();
+    bool result = true;
+
+    while (iterator->hasNext())
+        result = result && iterator->next()->initializeFlightMode();
+
+    return result;
+}
+
+
+bool VirtualPilot::setFlightMode(FlightModeTypes flightModeType)
+{
+    FlightMode* flightModeToSet = getFlightModePtrByType(flightModeType);
+
+    if (flightModeToSet == nullptr)
+        return false;
+
+    if (flightModeToSet == currentFlightMode)
+        return true;
+
+    executePrepareAndLeaveMethods(currentFlightMode, flightModeToSet);
+    
+    currentFlightMode = flightModeToSet;
+}
+
+
 void VirtualPilot::runVirtualPilot()
 {
     updateFlightModesSticksWithRmtCtrlValues();
@@ -28,29 +69,43 @@ void VirtualPilot::runVirtualPilot()
 }
 
 
-bool VirtualPilot::setFlightMode(FlightMode* flightModeToSet)
-{
-    if (flightModeToSet == nullptr)
-        return false;
-    
-    currentFlightMode = flightModeToSet;
-
-
-
-
-    // TODO: there is the hard part.
-    // need to call leave() and prepare() methods for certain flight modes.
-    // figure out how to do that without creating flight modes list here.
-    // maybe create that list in the constructor of FlightMode and this list
-    // will be a static field of that class.
-}
-
-
 FlightModeTypes VirtualPilot::getCurrentFlightModeType()
 {
     return currentFlightMode->getType();
 }
 
+
+
+FlightMode* VirtualPilot::getFlightModePtrByType(FlightModeTypes flightModeType)
+{
+    Iterator<FlightMode*>* iter = flightModesArray.getIterator();
+
+    while (iter->hasNext())
+    {
+        FlightMode* temp = iter->next();
+        if (temp->getType() == flightModeType)
+            return temp;
+    }
+
+    return nullptr;
+}
+
+
+void VirtualPilot::executePrepareAndLeaveMethods(const FlightMode* oldFlightMode, const FlightMode* newFlightMode)
+{
+    Iterator<FlightMode*>* iterator = flightModesArray.getIterator();
+    while (iterator->hasNext())
+    {
+        FlightMode* checked = iterator->next();
+        bool wasUsed = oldFlightMode->checkIfSuperiorOrEqualOf(checked);
+        bool willBeUsed = newFlightMode->checkIfSuperiorOrEqualOf(checked);
+
+        if (!wasUsed && willBeUsed)
+            checked->prepare();
+        else if (wasUsed && !willBeUsed)
+            checked->leave();
+    }
+}
 
 
 void VirtualPilot::updateFlightModesSticksWithRmtCtrlValues()
@@ -63,9 +118,9 @@ void VirtualPilot::updateFlightModesSticksWithRmtCtrlValues()
 void VirtualPilot::executeCurrentFlightMode()
 {
     if (motors->getMotorsState() == StateType::Enabled)
-        currentFlightMode->armedLoop();
+        currentFlightMode->executeArmedLoop();
     else
-        currentFlightMode->disarmedLoop();
+        currentFlightMode->executeDisarmedLoop();
 }
 
 
--- ColyberCopter/VirtualPilot.h
+++ ColyberCopter/VirtualPilot.h
@@ -39,6 +39,22 @@ public:
 
 private:
     /**
+     * @brief Get flight mode pointer from list of previously added ones by the type.
+     * @param flightModeType Type of flight mode to add.
+     * @return Pointer to the flight mode (previously added by addFlightMode() method)
+     * or nullptr if flight mode of that type was not found.
+     */
+    FlightMode* getFlightModePtrByType(Enums::FlightModeTypes flightModeType);
+
+    /**
+     * @brief Execute prepare() method for flight modes on the list that wasn't used and now will be
+     * and leave() method for flight modes that was used but now won't be.
+     * @param oldFlightMode flight mode that was previously set.
+     * @param newFlightMode flight mode that will be set from now.
+     */
+    void executePrepareAndLeaveMethods(const FlightMode* oldFlightMode, const FlightMode* newFlightMode);
+
+    /**
      * @brief Assign flight mode control sticks with data received
      * from the remote controller.
      */
