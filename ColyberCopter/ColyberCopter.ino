db08b00592691c8f75204e39397101800639c2ad
Jan Wielgus <jan.wielgus12@gmail.com>
2020-11-25 19:39:22 +0100

    SensorsMediator implementation

--- ColyberCopter/ColyberCopter.ino
+++ ColyberCopter/ColyberCopter.ino
@@ -1,5 +1,6 @@
 
 // TODO: when class want to use instance of other class, prefer passing by reference. Check if it is like that.
+// TODO: repair all singletons with reference to the instance. It may don't work and pointers are better there
 
 void setup()
 {
