b345c0a11ccc426e8e71bcfa80d45272cac69f7d
Jan Wielgus <jan.wielgus12@gmail.com>
2021-03-01 10:52:58 +0100

    Prepared file for MS5611 sensor adapter

---
+++ ColyberCopter/Sensors/MS5611Adapter.h
@@ -0,0 +1,16 @@
+/**
+ * @file MS5611Adapter.h
+ * @author Jan Wielgus (jan.wielgus12@gmail.com)
+ * @brief Adapter for MS5611 barometer.
+ * @date 2021-02-25
+ * 
+ */
+
+#ifndef MS5611ADAPTER_H
+#define MS5611ADAPTER_H
+
+#include <SimpleMS5611.h>
+
+
+
+#endif
