e934efd112b39d8dd89e85545275418d29aa13bf
Jan Wielgus <jan.wielgus12@gmail.com>
2021-03-04 22:16:01 +0100

    HMC5883LAdapter methods definition moved to separate cpp file

--- ColyberCopter/Sensors/HMC5883LAdapter.h
+++ ColyberCopter/Sensors/HMC5883LAdapter.h
@@ -31,34 +31,11 @@ private:
 
 
 public:
-    HMC5883LAdapter(SensorsMediator& sensorsMediator, SimpleMPU6050* mpu6050)
-        : Sensor(sensorsMediator)
-    {
-        mpu = mpu6050;
-    }
+    HMC5883LAdapter(SensorsMediator& sensorsMediator, SimpleMPU6050* mpu6050);
 
-    bool initialize() override
-    {
-        mpu->enableCompassBypass();
+    bool initialize() override;
 
-        int attempts = 0;
-        do {
-            initResult = compass.initialize();
-            attempts++;
-        } while (initResult == false && attempts < 3);
-
-        return initResult;
-    }
-
-    void execute() override
-    {
-        compass.readRaw();
-
-        checkCalibration();
-
-        SimpleHMC5883L::vector3Float norm = compass.getNormalized();
-        sensorsMediator.updateMag(vector3Float(norm.x, norm.y, norm.z));
-    }
+    void execute() override;
 
     /**
      * @brief During calibration make every possible rotation with a magnetometer.
@@ -66,64 +43,15 @@ public:
      * the calibration process.
      * @return Time that calibration process will take [in seconds].
      */
-    uint16_t startBackgroundCalibration(uint16_t amtOfSamples) override
-    {
-        compass.setCompassOffset(0, 0, 0);
+    uint16_t startBackgroundCalibration(uint16_t amtOfSamples) override;
 
-        const SimpleHMC5883L::vector3Int16& raw = compass.getRaw();
-        mins.x = maxs.x = raw.x;
-        mins.y = maxs.y = raw.y;
-        mins.z = maxs.z = raw.z;
+    FloatAxisVector getOffset() const override;
 
-        calibCounter.reset(amtOfSamples);
-
-        return getInterval_s() * amtOfSamples + 1;
-    }
-
-    FloatAxisVector getOffset() const override
-    {
-        const SimpleHMC5883L::vector3Int16& magOffset = compass.getCompassOffset();
-        return FloatAxisVector(3, magOffset.x, magOffset.y, magOffset.z);
-    }
-
-    void setOffset(FloatAxisVector offset) override
-    {
-        using Enums::AxisType;
-        compass.setCompassOffset(
-            offset.getAxis(AxisType::AxisX),
-            offset.getAxis(AxisType::AxisY),
-            offset.getAxis(AxisType::AxisZ));
-    }
-
+    void setOffset(FloatAxisVector offset) override;
 
 
 private:
-    void checkCalibration()
-    {
-        if (calibCounter.getCurrentCounter() > 0)
-        {
-            const SimpleHMC5883L::vector3Int16& raw = compass.getRaw();
-            mins.x = min(raw.x, mins.x);
-            mins.y = min(raw.y, mins.y);
-            mins.z = min(raw.z, mins.z);
-
-            maxs.x = max(raw.x, maxs.x);
-            maxs.y = max(raw.y, maxs.y);
-            maxs.z = max(raw.z, maxs.z);
-
-            // if calibration ends now
-            if (calibCounter.getCurrentCounter() == 1)
-            {
-                compass.setCompassOffset(
-                    (maxs.x + mins.x) / 2.f + 0.5f,
-                    (maxs.y + mins.y) / 2.f + 0.5f,
-                    (maxs.z + mins.z) / 2.f + 0.5f);
-            }
-
-            calibCounter.decrement();
-        }
-    }
-
+    void checkCalibration();
 };
 
 
