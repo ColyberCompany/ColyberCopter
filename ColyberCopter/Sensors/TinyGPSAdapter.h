97cf34d564ececd8044dba33ccd734312d98f373
antoniwielgus <antoniwielgus@gmail.com>
2021-07-05 20:16:24 +0200

    repaired

--- ColyberCopter/Sensors/TinyGPSAdapter.h
+++ ColyberCopter/Sensors/TinyGPSAdapter.h
@@ -1,27 +1,31 @@
+/**
+ * @file TinyGPSAdapter.h
+ * @author Antoni Wielgus
+ * @brief Adapter for TinyGPS++.
+ * @date 2021-07-05
+ * 
+ */
+
+
 #ifndef TINYGPSADAPTER_H
 #define TINYGPSADAPTER_H
 
 #include "Sensor.h"
-#include "SensorsMediator.h"
 #include <IExecutable.h>
 #include <TinyGPS++.h>
-#include <HardwareSerial.h>
 
 
 class TinyGPSAdapter: public Sensor, public IExecutable
 {
     TinyGPSPlus gps;
+    Stream& gpsSerial;
 
 public:
-    TinyGPSAdapter(SensorsMediator& SensorsMediator);
+    TinyGPSAdapter(SensorsMediator& SensorsMediator, Stream& gpsSerial);
 
     void execute() override;
-
     bool initialize() override;
-    const char* getName() override
-    {
-        return "GPS";
-    }
+    const char* getName() override;
 };
 
 
--- ColyberCopter/src/TinyGPSAdapter.cpp
+++ ColyberCopter/src/TinyGPSAdapter.cpp
@@ -1,31 +1,46 @@
+/**
+ * @file TinyGPSAdapter.h
+ * @author Antoni Wielgus
+ * @date 2021-07-05
+ * 
+ */
+
+
 #include "../Sensors/TinyGPSAdapter.h"
 #include "../Enums/BaudRateTypes.h"
 
-TinyGPSAdapter::TinyGPSAdapter(SensorsMediator& sensorsMediator)
-    : Sensor(Enums::SensorTypes::GPS, sensorsMediator)
+TinyGPSAdapter::TinyGPSAdapter(SensorsMediator& sensorsMediator, Stream& _gpsSerial)
+    : Sensor(Enums::SensorTypes::GPS, sensorsMediator), gpsSerial(_gpsSerial)
 {
 }
 
 bool TinyGPSAdapter::initialize()
 {
-    Serial2.begin(Enums::BAUD_9600);
+    gpsSerial.begin(Enums::BAUD_9600);
 
-    if(gps.charsProcessed() < 10 && gps.encode(Serial.read()) == 0) 
-        return false;
-    
-    return true;
+    // TODO: implementation
+/*
+    if(gps.charsProcessed() < 10 && gps.encode(gpsSerial.read()) == 0)
+        initResultFlag = false;
+    else
+        initResultFlag = true;
+
+    return initResultFlag;
+*/
+
+    return false;
 }
 
 void TinyGPSAdapter::execute()
 {
-    while(Serial2.available() > 0)
+    while(gpsSerial.available() > 0)
     {
-        if(gps.encode(Serial2.read()))
-        {
-            if(gps.location.isValid())
-            {
-                sensorsMediator.updateLatLong(gps.location.lat(), gps.location.lng());
-            }    
-        }      
+        if(gps.encode(gpsSerial.read()) && gps.location.isValid())
+            sensorsMediator.updateLatLong(gps.location.lat(), gps.location.lng());        
     }   
+}
+
+const char* getName() override
+{
+    return "GPS";
 }
