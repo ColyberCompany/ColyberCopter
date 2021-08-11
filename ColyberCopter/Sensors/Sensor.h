36bb52231007af23af302c517f1cf8cec3636fcf
Jan Wielgus <jan.wielgus12@gmail.com>
2021-08-11 18:44:11 +0200

    Updated Sensors class

--- ColyberCopter/Sensors/Sensor.h
+++ ColyberCopter/Sensors/Sensor.h
@@ -9,7 +9,6 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
-#include "../Common/FloatAxisVector.h"
 #include "../Enums/SensorTypes.h"
 
 
@@ -27,7 +26,6 @@ public:
 
     Sensor(const Sensor&) = delete;
     Sensor& operator=(const Sensor&) = delete;
-
     virtual ~Sensor() {}
 
     /**
@@ -50,6 +48,14 @@ public:
     }
 
     /**
+     * @brief Get type of the current sensor.
+     */
+    Enums::SensorTypes getType()
+    {
+        return type;
+    }
+
+    /**
      * @brief Check if sensor is currently updating new measurements in sensorsMediator.
      * @return true if sensor is running.
      */
@@ -62,45 +68,6 @@ public:
      * @brief Name getter of this sensor.
      */
     virtual const char* getName() = 0;
-
-    /**
-     * @brief Get type of the current sensor.
-     */
-    Enums::SensorTypes getType()
-    {
-        return type;
-    }
-
-    /**
-     * @brief After calling this method sensor will begin calibration during
-     * it's normal execution and after that it will apply new offset values.
-     * @param amtOfSamples Amount of samples that for example are averaged during calibration
-     * (bigger amount -> longer calibration process and better results).
-     * @return Amount of seconds that the calibration process will take.
-     * You can call getOffset() method after that time and get new offset values.
-     */
-    virtual uint16_t startBackgroundCalibration(uint16_t amtOfSamples) // TODO: calibration process to rebuild (issue on github)
-    {
-        // Don't need to calibrate by default
-        return 0;
-    }
-
-    /**
-     * @brief Getter of the calibration values.
-     * @return Current calibration values.
-     */
-    virtual Common::FloatAxisVector getOffset() const
-    {
-        return Common::FloatAxisVector();
-    }
-
-    /**
-     * @brief Setter of the calibration values.
-     * @param offset Calibration values (offset) to set.
-     */
-    virtual void setOffset(Common::FloatAxisVector offset)
-    {
-    }
 
 
 private:
