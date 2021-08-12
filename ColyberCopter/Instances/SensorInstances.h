6445d39988973f941289b3d22fa22dfb508a9269
Jan Wielgus <jan.wielgus12@gmail.com>
2021-08-12 14:30:42 +0200

    Updated lots of code with prev changes

--- ColyberCopter/Instances/SensorInstances.h
+++ ColyberCopter/Instances/SensorInstances.h
@@ -3,24 +3,26 @@
  * @author Jan Wielgus (jan.wielgus12@gmail.com)
  * @brief Instances of all sensor classes.
  * @date 2021-03-29
- * 
  */
 
 
 #ifndef SENSORINSTANCES_H
 #define SENSORINSTANCES_H
 
-#include "../Sensors/Sensor.h"
+#include "Sensors/Base/Accelerometer.h"
+#include "Sensors/Base/Gyroscope.h"
+#include "Sensors/Base/Magnetometer.h"
+#include "Sensors/Base/Barometer.h"
+#include "Sensors/Base/GPS.h"
+#include "Sensors/Base/Rangefinder.h"
 
 
 namespace Instance
 {
-    extern Sensor& accel;
-    extern Sensor& gyro;
-    extern Sensor& magn;
-    extern Sensor& baro;
-    extern Sensor& gps;
-    extern Sensor& btmRangefinder;
+    extern Accelerometer& acc;
+    extern Gyroscope& gyro;
+    extern Magnetometer& magn;
+    //extern Barometer& baro;
     // extern other sensors...
 }
 
