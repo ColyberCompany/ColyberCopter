207d9654c844fbe3969655266e879cd6d8480942
Jan Wielgus <jan.wielgus12@gmail.com>
2021-08-18 15:46:56 +0200

    Updated utils

--- ColyberCopter/Common/Utils.h
+++ ColyberCopter/Common/Utils.h
@@ -8,7 +8,7 @@ namespace Common
     namespace Utils
     {
         template <typename T>
-        void printVector3(Stream& serialNumber,const vector3<T>& toShow, const char* separator)
+        void printVector3(Stream& serialNumber,const vector3<T>& toShow, const char* separator = "\t")
         {
             serialNumber.print(toShow.x);
             serialNumber.print(separator);
@@ -17,15 +17,14 @@ namespace Common
             serialNumber.println(toShow.z);
         }
 
-        template <typename T>
-        void printVector3(Stream& serialNumber,const vector3<T>& toShow, char separator = '\t')
-        {
-            serialNumber.print(toShow.x);
-            serialNumber.print(separator);
-            serialNumber.print(toShow.y);
-            serialNumber.print(separator);
-            serialNumber.println(toShow.z);
-        }
+
+        /**
+         * @brief Fast inverse square-root
+         * See: http://en.wikipedia.org/wiki/Fast_inverse_square_root.
+         * @param x Number which square root will be extracted.
+         * @return Square root of parameter x.
+         */
+        float invSqrt(float x);
     }
 }
 
