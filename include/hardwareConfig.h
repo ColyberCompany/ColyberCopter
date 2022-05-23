19e003830867997ea706228a0d0a84b5d5e3db63
Jan Wielgus <jan.wielgus12@gmail.com>
2022-05-23 20:39:47 +0200

    Refactored hardwareConfig.h

--- include/hardwareConfig.h
+++ include/hardwareConfig.h
@@ -9,72 +9,64 @@
 #define CONDITIONALCOMPILATION_h
 
 
-	// Available sensors
-#define COLYBER_SENSOR_NO_SENSOR		0
-#define COLYBER_SENSOR_MPU6050			1	// Acc + Gyro + Temperature
-#define COLYBER_SENSOR_MPU6500SPI		2	// Acc + Gyro + Temperature
-#define COLYBER_SENSOR_HMC5883L			3	// Magn
-#define COLYBER_SENSOR_TINY_GPS_PLUS	4	// Gps
+// Available sensors
+#define COLYBER_SENSOR_NO_SENSOR            0
+#define COLYBER_SENSOR_MPU6050              1   // Acc + Gyro + Temperature
+#define COLYBER_SENSOR_MPU6500SPI           2   // Acc + Gyro + Temperature
+#define COLYBER_SENSOR_HMC5883L             3   // Magn
+#define COLYBER_SENSOR_TINY_GPS_PLUS        4   // Gps
 // add new sensor here...
 
 
-	// Sensors selection (change sensor by setting proper value)
-#define COLYBER_MAGN			COLYBER_SENSOR_NO_SENSOR			// TODO: calibrate magnetometer
-#define COLYBER_2ND_ACC			COLYBER_SENSOR_NO_SENSOR			// TODO: not implemented yet
-#define COLYBER_2ND_GYRO		COLYBER_SENSOR_NO_SENSOR			// TODO: not implemented yet
-#define COLYBER_GPS				COLYBER_SENSOR_TINY_GPS_PLUS
+// Sensors selection (change sensor by setting proper value)
+#define COLYBER_MAGN                COLYBER_SENSOR_NO_SENSOR    // TODO: calibrate magnetometer
+#define COLYBER_2ND_ACC             COLYBER_SENSOR_NO_SENSOR    // TODO: not implemented yet
+#define COLYBER_2ND_GYRO            COLYBER_SENSOR_NO_SENSOR    // TODO: not implemented yet
+#define COLYBER_GPS                 COLYBER_SENSOR_TINY_GPS_PLUS
 
-
-	// Other flags (comment if want to deactivate)
-// #define COLYBER_DEACTIVATE_MOTORS						// Motors won't spin
-
+// Other flags (comment if want to deactivate)
+// #define COLYBER_DEACTIVATE_MOTORS                // Motors won't spin
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
 
-	// Auto flags deduction (do not comment)
+// Auto flags deduction (do not comment)
 #if COLYBER_MAGN != COLYBER_SENSOR_NO_SENSOR
-	#define COLYBER_USE_MAGN
+#define COLYBER_USE_MAGN
 #endif
 #if COLYBER_2ND_ACC != COLYBER_SENSOR_NO_SENSOR
-	#define COLYBER_USE_2ND_ACC
+#define COLYBER_USE_2ND_ACC
 #endif
 #if COLYBER_2ND_GYRO != COLYBER_SENSOR_NO_SENSOR
-	#define COLYBER_USE_2ND_GYRO
+#define COLYBER_USE_2ND_GYRO
 #endif
 #if COLYBER_GPS != COLYBER_SENSOR_NO_SENSOR
-	#define COLYBER_USE_GPS
+#define COLYBER_USE_GPS
 #endif
 
-
-	// Validation (do not comment)
-#if !( \
-	COLYBER_2ND_ACC == COLYBER_SENSOR_NO_SENSOR || \
-	COLYBER_2ND_ACC == COLYBER_SENSOR_MPU6050 || \
-	COLYBER_2ND_ACC == COLYBER_SENSOR_MPU6500SPI \
-	/* other accelerometers */ )
-	#error "Invalid accelerometer"
+// Validation (do not comment)
+#if !(                                             \
+    COLYBER_2ND_ACC == COLYBER_SENSOR_NO_SENSOR || \
+    COLYBER_2ND_ACC == COLYBER_SENSOR_MPU6050 ||   \
+    COLYBER_2ND_ACC == COLYBER_SENSOR_MPU6500SPI /* other accelerometers */)
+#error "Invalid accelerometer"
 #endif
-#if !( \
-	COLYBER_2ND_GYRO == COLYBER_SENSOR_NO_SENSOR || \
-	COLYBER_2ND_GYRO == COLYBER_SENSOR_MPU6050 || \
-	COLYBER_2ND_GYRO == COLYBER_SENSOR_MPU6500SPI \
-	/* other gyroscopes */ )
-	#error "Invalid gyroscope"
+#if !(                                              \
+    COLYBER_2ND_GYRO == COLYBER_SENSOR_NO_SENSOR || \
+    COLYBER_2ND_GYRO == COLYBER_SENSOR_MPU6050 ||   \
+    COLYBER_2ND_GYRO == COLYBER_SENSOR_MPU6500SPI /* other gyroscopes */)
+#error "Invalid gyroscope"
 #endif
-#if !( \
-	COLYBER_MAGN == COLYBER_SENSOR_NO_SENSOR || \
-	COLYBER_MAGN == COLYBER_SENSOR_HMC5883L \
-	/* other magnetometers... */ )
-	#error "Invalid magnetometer"
+#if !(                                          \
+    COLYBER_MAGN == COLYBER_SENSOR_NO_SENSOR || \
+    COLYBER_MAGN == COLYBER_SENSOR_HMC5883L /* other magnetometers... */)
+#error "Invalid magnetometer"
 #endif
-#if !( \
-	COLYBER_GPS == COLYBER_SENSOR_NO_SENSOR || \
-	COLYBER_GPS == COLYBER_SENSOR_TINY_GPS_PLUS \
-	/* other gps sensors... */ )
-	#error "Invalid gps sensor"
+#if !(                                         \
+    COLYBER_GPS == COLYBER_SENSOR_NO_SENSOR || \
+    COLYBER_GPS == COLYBER_SENSOR_TINY_GPS_PLUS /* other gps sensors... */)
+#error "Invalid gps sensor"
 #endif
-
 
 #endif // CONDITIONALCOMPILATION_h
