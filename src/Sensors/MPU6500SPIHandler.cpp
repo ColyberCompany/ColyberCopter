fbe07464741a103fe4b97b2008f68a0f522d3bf8
Jan Wielgus <jan.wielgus12@gmail.com>
2022-05-31 11:56:29 +0200

    Added acc filtering to MPU6500SPIHandler

--- src/Sensors/MPU6500SPIHandler.cpp
+++ src/Sensors/MPU6500SPIHandler.cpp
@@ -5,12 +5,19 @@
  */
 
 #include "MPU6500SPIHandler.h"
+#include "config.h"
 
 static const uint32_t SpiClock = 8000000l; // 8MHz clock (not tried 20MHz)
+constexpr float AccLPFCutOffFreq = 8.f;
 
 
 MPU6500SPIHandler::MPU6500SPIHandler(SPIClass& bus, uint8_t csPin):
-    mpu(bus, SpiClock, csPin)
+    mpu(bus, SpiClock, csPin),
+    accLPF(
+        {AccLPFCutOffFreq, Config::MainInterval_s},
+        {AccLPFCutOffFreq, Config::MainInterval_s},
+        {AccLPFCutOffFreq, Config::MainInterval_s}
+    )
 {
 }
 
@@ -23,7 +30,7 @@ bool MPU6500SPIHandler::init_priv()
     if (!mpu.initialize())
         return false;
 
-    mpu.setAccRange(MPU6500SPI::AccRange::Range_8G);
+    mpu.setAccRange(MPU6500SPI::AccRange::Range_4G);
     mpu.setGyroScale(MPU6500SPI::GyroScale::Scale500DPS);
 
     return true;
@@ -33,4 +40,11 @@ bool MPU6500SPIHandler::init_priv()
 void MPU6500SPIHandler::execute()
 {
     mpu.readAll();
+
+    auto acc = mpu.getNormalizedAcceleration();
+    accFiltered = Common::vector3Float(
+        accLPF.x.update(acc.x),
+        accLPF.y.update(acc.y),
+        accLPF.z.update(acc.z)
+    );
 }
