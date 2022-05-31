56f0d759e9855b924e738ac1f76a83b8e9f01c69
Jan Wielgus <jan.wielgus12@gmail.com>
2022-05-31 14:38:34 +0200

    Updated calibration in MPU6500SPIHandler

--- src/Sensors/MPU6500SPIHandler.cpp
+++ src/Sensors/MPU6500SPIHandler.cpp
@@ -10,6 +10,14 @@
 static const uint32_t SpiClock = 8000000l; // 8MHz clock (not tried 20MHz)
 constexpr float AccLPFCutOffFreq = 8.f;
 
+// TODO: figure out place for calibration values (for specific hardware)
+const FusionMatrix accMisalignment = {0.997712, -0.003394, 0.001708, -0.003394, 0.996472, 0.000751, 0.001708, 0.000751, 0.984676};
+const FusionVector accSensitivity = {1.0f, 1.0f, 1.0f};
+const FusionVector accOffset = {0.013689, 0.009321, -0.102916};
+const FusionMatrix gyroMisalignment = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+const FusionVector gyroSensitivity = {1.0f, 1.0f, 1.0f};
+const FusionVector gyroOffset = {0.0f, 0.0f, 0.0f};
+
 
 MPU6500SPIHandler::MPU6500SPIHandler(SPIClass& bus, uint8_t csPin):
     mpu(bus, SpiClock, csPin),
@@ -19,6 +27,16 @@ MPU6500SPIHandler::MPU6500SPIHandler(SPIClass& bus, uint8_t csPin):
         {AccLPFCutOffFreq, Config::MainInterval_s}
     )
 {
+    accelerometer().setCalibration(CalibrationIntertial::Calibration {
+        .misalignment = accMisalignment,
+        .sensitivity = accSensitivity,
+        .offset = accOffset
+    });
+    gyroscope().setCalibration(CalibrationIntertial::Calibration {
+        .misalignment = gyroMisalignment,
+        .sensitivity = gyroSensitivity,
+        .offset = gyroOffset
+    });
 }
 
 
